2641fa004f6a0f5c0a4f70931db3870d1682d279
karthikmadhavjain <70726547+karthikmadhavjain@users.noreply.github.com>
2020-09-03 19:21:52 -0400

    Delete main.py

--- main.py
+++
@@ -1,3 +0,0 @@
-tempc = float (input('Enter temperature in celsius: '))
-tempf = ((tempc*(9/5))+32)
-print (str(tempc)+"\N{DEGREE SIGN}",'in Celsius is equivalent to',str(tempf)+"\N{DEGREE SIGN}",'Fahrenheit.')
