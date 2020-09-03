1ac2e312f90f7ed7d15e48d52fd77612c4aeb10f
karthikmadhavjain <70726547+karthikmadhavjain@users.noreply.github.com>
2020-09-03 18:28:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-tempc = float (input('Enter temperature in celcius:'))
+tempc = float (input('Enter temperature in celcius: '))
 tempf = ((tempc*(9/5))+32)
 print (str(tempc), 'in Celsius is equivalent to', str(tempf), 'Fahrenheit.')
